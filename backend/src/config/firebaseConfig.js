47bfdcbdf3d373650d740b55f665a758a1722f1e
hiba <hebaaltayeb2@icloud.com>
2024-11-01 05:28:32 +0300

    🗑️ Remove firebaseConfig.js to clean up history

--- backend/src/config/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./curatool-a9049-firebase-adminsdk-iwve7-76db36b0e8.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
