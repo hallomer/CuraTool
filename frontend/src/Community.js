7acbbc1bea67ce218371f06edbdb11c0a2447826
hiba <hebaaltayeb2@icloud.com>
2024-11-05 08:42:01 +0300

    ✍🏾 Add message for empty posts/comments

--- frontend/src/Community.js
+++ frontend/src/Community.js
@@ -212,125 +212,133 @@ const Community = () => {
                 </div>
             )}
             <div className="posts">
-                {posts.map((post) => {
-                    const userRating = userRatings[post._id];
-                    const upArrowColor = userRating === 1 ? '#FFA07A' : '#666';
-                    const downArrowColor = userRating === -1 ? '#FFA07A' : '#666';
-                    return (
-                        <div key={post._id} className="post">
-                            <div className="post-header">
-                                <img src={post.user.profilePicture} alt={post.user.username} />
-                                <span className="username">{post.user.username}</span>
-                                <span className="post-date">{formatDate(post.createdAt)}</span>
-                                {user && user.uid === post.user.uid && (
-                                        <FaTrash className="delete-icon" onClick={() => handleDeletePost(post._id)} />
+                {posts.length === 0 ? (
+                    <p className="no-content-message">No posts here yet! Be the first to share your ideas, questions, or guides.</p>
+                ) : ( 
+                    posts.map((post) => {
+                        const userRating = userRatings[post._id];
+                        const upArrowColor = userRating === 1 ? '#FFA07A' : '#666';
+                        const downArrowColor = userRating === -1 ? '#FFA07A' : '#666';
+                        return (
+                            <div key={post._id} className="post">
+                                <div className="post-header">
+                                    <img src={post.user.profilePicture} alt={post.user.username} />
+                                    <span className="username">{post.user.username}</span>
+                                    <span className="post-date">{formatDate(post.createdAt)}</span>
+                                    {user && user.uid === post.user.uid && (
+                                            <FaTrash className="delete-icon" onClick={() => handleDeletePost(post._id)} />
+                                    )}
+                                </div>
+                                <p>{post.text}</p>
+                                {post.image && (
+                                <img
+                                    src={`${process.env.REACT_APP_UPLOADS_BASE_URL}uploads/${post.image}`}
+                                    alt="Post"
+                                    className="post-image"
+                                    onClick={() => handleImageClick(`${process.env.REACT_APP_UPLOADS_BASE_URL}uploads/${post.image}`)}
+                                    style={{ cursor: "pointer" }}
+                                />
                                 )}
-                            </div>
-                            <p>{post.text}</p>
-                            {post.image && (
-                              <img
-                                src={`${process.env.REACT_APP_UPLOADS_BASE_URL}uploads/${post.image}`}
-                                alt="Post"
-                                className="post-image"
-                                onClick={() => handleImageClick(`${process.env.REACT_APP_UPLOADS_BASE_URL}uploads/${post.image}`)}
-                                style={{ cursor: "pointer" }}
-                              />
-                            )}
-                            <div className="post-actions">
-                                <div className="rating-buttons">
-                                    <button
-                                        className="rating-button"
-                                        onClick={() => handleRatePost(post._id, 1)}
-                                        style={{ color: upArrowColor }}
-                                    >
-                                        ▲
-                                    </button>
-                                    <span className="rating-count">{post.totalRating || 0}</span>
-                                    <button
-                                        className="rating-button"
-                                        onClick={() => handleRatePost(post._id, -1)}
-                                        style={{ color: downArrowColor }}
-                                    >
-                                        ▼
+                                <div className="post-actions">
+                                    <div className="rating-buttons">
+                                        <button
+                                            className="rating-button"
+                                            onClick={() => handleRatePost(post._id, 1)}
+                                            style={{ color: upArrowColor }}
+                                        >
+                                            ▲
+                                        </button>
+                                        <span className="rating-count">{post.totalRating || 0}</span>
+                                        <button
+                                            className="rating-button"
+                                            onClick={() => handleRatePost(post._id, -1)}
+                                            style={{ color: downArrowColor }}
+                                        >
+                                            ▼
+                                        </button>
+                                    </div>
+                                    <button className="comments-button" onClick={() => toggleComments(post._id)}>
+                                        <FaComments /> Comments
                                     </button>
                                 </div>
-                                <button className="comments-button" onClick={() => toggleComments(post._id)}>
-                                    <FaComments /> Comments
-                                </button>
-                            </div>
-                            {expandedPosts[post._id] && (
-                                <div className="comments">
-                                    {post.comments.map((comment) => (
-                                        <div key={comment._id} className="comment">
-                                            <div className="comment-header">
-                                                <img src={comment.user.profilePicture} alt={comment.user.username} />
-                                                <span className="username">{comment.user.username}</span>
-                                                <span className="comment-date">{formatDate(comment.createdAt)}</span>
-                                                {user && comment.user && user.uid === comment.user.uid && (
-                                                    <FaTrash className="delete-icon" onClick={() => handleDeleteComment(post._id, comment._id)} />
-                                                )}
+                                {expandedPosts[post._id] && (
+                                    <div className="comments">
+                                        {post.comments.length === 0 ? (
+                                            <p className="no-content-message">No comments yet—join the conversation and share your thoughts!</p>
+                                        ) : (
+                                            post.comments.map((comment) => (
+                                                <div key={comment._id} className="comment">
+                                                    <div className="comment-header">
+                                                        <img src={comment.user.profilePicture} alt={comment.user.username} />
+                                                        <span className="username">{comment.user.username}</span>
+                                                        <span className="comment-date">{formatDate(comment.createdAt)}</span>
+                                                        {user && comment.user && user.uid === comment.user.uid && (
+                                                            <FaTrash className="delete-icon" onClick={() => handleDeleteComment(post._id, comment._id)} />
+                                                        )}
+                                                    </div>
+                                                    <div className="comment-body">
+                                                    <p>{comment.text}</p>
+                                                    {comment.image && (
+                                                    <img
+                                                        className="comment-image"
+                                                        src={`${process.env.REACT_APP_UPLOADS_BASE_URL}uploads/${comment.image}`}
+                                                        alt="Comment"
+                                                        onClick={() => handleImageClick(`${process.env.REACT_APP_UPLOADS_BASE_URL}uploads/${comment.image}`)}
+                                                        style={{ cursor: "pointer" }}
+                                                    />
+                                                    )}
+                                                    </div>
+                                                </div>
+                                            ))
+                                        )}
+                                        <div className="add-comment">
+                                            <textarea
+                                                placeholder="Write a comment..."
+                                                value={newCommentData[post._id]?.text || ""}
+                                                onChange={(e) => handleCommentInputChange(post._id, 'text', e.target.value)}
+                                                onInput={(e) => {
+                                                    e.target.style.height = "auto";
+                                                    e.target.style.height = `${e.target.scrollHeight}px`;
+                                                }}
+                                                className="comment-textarea"
+                                                onClick={() => {
+                                                    if (!user) window.location.href = "/login";
+                                                }}
+                                                readOnly={!user}
+                                            />
+                                            <div className="add-comment-row">
+                                            <label
+                                                htmlFor={`file-upload-${post._id}`}
+                                                className="file-upload-label"
+                                                onClick={() => {
+                                                    if (!user) window.location.href = "/login";
+                                                }}
+                                            >
+                                                <FaImage />
+                                            </label>
+                                            <input
+                                                type="file"
+                                                id={`file-upload-${post._id}`}
+                                                style={{ display: 'none' }}
+                                                onChange={(e) => handleCommentInputChange(post._id, 'image', e.target.files[0])}
+                                                disabled={!user}
+                                            />
+                                            <button
+                                                onClick={() => {
+                                                    if (user) handleAddComment(post._id);
+                                                    else window.location.href = "/login";
+                                                }}
+                                            >
+                                                Add Comment
+                                            </button>
                                             </div>
-                                            <div className="comment-body">
-                                            <p>{comment.text}</p>
-                                            {comment.image && (
-                                              <img
-                                                className="comment-image"
-                                                src={`${process.env.REACT_APP_UPLOADS_BASE_URL}uploads/${comment.image}`}
-                                                alt="Comment"
-                                                onClick={() => handleImageClick(`${process.env.REACT_APP_UPLOADS_BASE_URL}uploads/${comment.image}`)}
-                                                style={{ cursor: "pointer" }}
-                                              />
-                                            )}
-                                            </div>
-                                        </div>
-                                    ))}
-                                    <div className="add-comment">
-                                        <textarea
-                                            placeholder="Write a comment..."
-                                            value={newCommentData[post._id]?.text || ""}
-                                            onChange={(e) => handleCommentInputChange(post._id, 'text', e.target.value)}
-                                            onInput={(e) => {
-                                                e.target.style.height = "auto";
-                                                e.target.style.height = `${e.target.scrollHeight}px`;
-                                            }}
-                                            className="comment-textarea"
-                                            onClick={() => {
-                                                if (!user) window.location.href = "/login";
-                                            }}
-                                            readOnly={!user}
-                                        />
-                                        <div className="add-comment-row">
-                                        <label
-                                            htmlFor={`file-upload-${post._id}`}
-                                            className="file-upload-label"
-                                            onClick={() => {
-                                                if (!user) window.location.href = "/login";
-                                            }}
-                                        >
-                                            <FaImage />
-                                        </label>
-                                        <input
-                                            type="file"
-                                            id={`file-upload-${post._id}`}
-                                            style={{ display: 'none' }}
-                                            onChange={(e) => handleCommentInputChange(post._id, 'image', e.target.files[0])}
-                                            disabled={!user}
-                                        />
-                                        <button
-                                            onClick={() => {
-                                                if (user) handleAddComment(post._id);
-                                                else window.location.href = "/login";
-                                            }}
-                                        >
-                                            Add Comment
-                                        </button>
                                         </div>
                                     </div>
-                                </div>
-                            )}
-                        </div>
-                    );
-                })}
+                                )}
+                            </div>
+                        );
+                    })
+                )}
             </div>
             {showImageModal && (
             <div className="image-modal" onClick={closeImageModal}>
